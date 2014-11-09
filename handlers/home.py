86945e36463037e32be1d65bce72b20ab5b3b085
icbrahimc <icbrahimc@gmail.com>
2014-11-09 03:29:00 -0500

    Update home.py

--- handlers/home.py
+++ handlers/home.py
@@ -6,6 +6,7 @@ from google.appengine.ext import ndb
 class Message(ndb.Model):
   person = ndb.StringProperty(required=True)
   content = ndb.StringProperty(required=True)
+  team = ndb.StringProperty(required = True)
 
 class Handler(webapp2.RequestHandler):
 
@@ -14,11 +15,17 @@ class Handler(webapp2.RequestHandler):
     
     name = self.request.get('person')
     messages = Message.query()
-    template_values = {'name': 'Prattle', 'date': 'Today', 'messages': messages}
+    messages1 = Message.query(Message.person==team)
+    template_values = {
+      'name': 'Prattle', 
+      'date': 'Today', 
+      'messages': messages,
+      'messages1': messages1,
+    }
     render.TemplateResponse('home.html', template_values, self.response)
 
   def post(self):
     name = self.request.get('moniker')
     message = self.request.get('message')
     Message(person=name, content=message).put()
-    render.TextResponse(name +"said" + message, self.response)
+    render.TextResponse(name +"said " + message, self.response)                                                        
